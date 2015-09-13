bfdf443e5588077b7f492a1cc44fab74c66d4396
iFresh <sergjj-don@mail.ru>
2015-09-13 14:57:28 +0300

    Initial commit

---
+++ src/com/labka/kalkulator/lab1/FirstApp.java
@@ -0,0 +1,12 @@
+package com.labka.kalkulator.lab1;
+
+/**
+ * Created by UNDERTAKERR on 13.09.2015.
+ */
+public class FirstApp {
+
+
+    public static void main(String[] args) {
+        System.out.println( 3 + 4 + 1 + 5 + 2 );
+    }
+}
